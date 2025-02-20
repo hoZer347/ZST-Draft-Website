c31e21e99e6651cedf6b2b20c8e40112f8164942
hoZer347 <34010435+hoZer347@users.noreply.github.com>
2025-02-20 14:26:18 -0500

    Added more .csv Support

--- Scripts/import_data.py
+++ Scripts/import_data.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
 SHEET_ID = "148NrapQQCjn7gGvRBhU90gIKAky1h9WRoGuCvANqMdE"
-SHEET_NAME = "Pokedex"
-CSV_URL = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/gviz/tq?tqx=out:csv&sheet={SHEET_NAME}"
 
-df = pd.read_csv(CSV_URL)
+def load_table(table_name):
+	csv_url = f"https://docs.google.com/spreadsheets/d/{ SHEET_ID }/gviz/tq?tqx=out:csv&sheet={ table_name }"
 
-# Convert NaN to empty string using `.map()` (Future-proof)
-df = df.map(lambda x: "" if pd.isna(x) else x)
+	df = pd.read_csv(csv_url)
+	df = df.applymap(lambda x: "" if pd.isna(x) else x)
+	df.to_csv(f"Data/{ table_name }.csv", index=False, encoding="utf-8")
 
-# Save as CSV
-df.to_csv("Data/Pokedex.csv", index=False, encoding="utf-8")
+	print(f"{ table_name } successfully saved to { table_name }.csv!")
 
-print("Pokedex successfully saved to Pokedex.csv!")
+load_table("Pokedex")
+load_table("Draft")
